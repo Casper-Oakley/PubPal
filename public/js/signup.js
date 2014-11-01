ff1d8dfe0b5dcbc3548b7fea9d7390bbfad70d0c
casper oakley <casper_oakley@hotmail.com>
2014-11-01 23:47:59 +0000

    minor

--- public/js/signup.js
+++ public/js/signup.js
@@ -3,7 +3,7 @@ var socket = io.connect(host, {secure: true});
 window.onload = function(){
 	$('#submit').click( function(){
 		socket.emit('signup', {username : $('#username').val(),password : $('#password').val(), number : $('#number').val()});
-		document.cookie=JSON.stringify({username : $('#username').val(),password : $('#password').val()});
+		document.cookie=JSON.stringify($('#username').val());
 		window.location.href="/home";
 	});
 }
