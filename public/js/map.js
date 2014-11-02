28ef8bb0e79a504fa35a5f16a666a81c5ab09a1c
Bericender <lgxge@nottingham.ac.uk>
2014-11-02 01:43:45 +0000

    show nearby places

--- public/js/map.js
+++ public/js/map.js
@@ -1,4 +1,5 @@
 var map;
+var infowindow;
 var directionsService = new google.maps.DirectionsService();
 var directionsDisplay = new google.maps.DirectionsRenderer();
 
@@ -8,24 +9,24 @@ function initialize() {
   };
 
   var map = new google.maps.Map(document.getElementById('map-canvas'),
-      mapOptions);
+  mapOptions);
 
   //geolocation attempt via HTML5
   if(navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(function(position) {
       var coords = new google.maps.LatLng(position.coords.latitude,
-                                       position.coords.longitude);
-      var directionsService = new google.maps.DirectionsService();
-      var directionsDisplay = new google.maps.DirectionsRenderer();
-      var mapOptions = //Sets map options
+        position.coords.longitude);
+        var directionsService = new google.maps.DirectionsService();
+        var directionsDisplay = new google.maps.DirectionsRenderer();
+        var mapOptions = //Sets map options
         {
           zoom: 15,  //Sets zoom level (0-21)
           center: coords, //zoom in on users location
           mapTypeControl: true, //allows you to select map type eg. map or satellite
           navigationControlOptions:
-            {
-              style: google.maps.NavigationControlStyle.SMALL //sets map controls size eg. zoom
-            },
+          {
+            style: google.maps.NavigationControlStyle.SMALL //sets map controls size eg. zoom
+          },
           mapTypeId: google.maps.MapTypeId.ROADMAP //sets type of map Options:ROADMAP, SATELLITE, HYBRID, TERRIAN
         };
 
@@ -41,12 +42,40 @@ function initialize() {
       directionsService.route(request, function (response, status) {
         if (status == google.maps.DirectionsStatus.OK) {
           directionsDisplay.setDirections(response);
-        }
-      });
+          }
+        });
 
-    });
- }
+      var request = {
+        location: coords,
+        radius: 500,
+        types: ['store']
+      };
+      infowindow = new google.maps.InfoWindow();
+      var service = new google.maps.places.PlacesService(map);
+      service.nearbySearch(request, callback);
+  });
+}
 
+function callback(results, status) {
+  if (status == google.maps.places.PlacesServiceStatus.OK) {
+    for (var i = 0; i < results.length; i++) {
+      createMarker(results[i]);
+    }
+  }
+}
+
+function createMarker(place) {
+  var placeLoc = place.geometry.location;
+  var marker = new google.maps.Marker({
+    map: map,
+    position: place.geometry.location
+  });
+
+google.maps.event.addListener(marker, 'click', function() {
+    infowindow.setContent(place.name);
+    infowindow.open(map, this);
+  });
+}
   setMarkers(map,pubs);
 
 }
