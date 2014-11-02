b3591179b14930740328440f5e623b01e0387a11
casper oakley <casper_oakley@hotmail.com>
2014-11-02 00:54:59 +0000

    img

--- public/js/index.js
+++ public/js/index.js
@@ -2,14 +2,18 @@ var host = location.origin.replace(/^http/,'ws');
 var socket = io.connect(host, {secure: true});
 window.onload = function(){
 	$('#submit').click( function(){
-		socket.emit('login',{username : $('#username').val(),password : $('#password').val()});
+		if($('#username').val().search(/[^A-Za-z0-9]/)==-1 && $('#password').val().search(/[^A-Za-z0-9]/==-1){
+			socket.emit('login',{username : $('#username').val(),password : $('#password').val()});
+		}
+		else{
+			$('#alertlocation').after('<div class="alert alert-danger alert-dismissable" id="failed"><button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button> ERROR: Invalid Login. </div>');
+		}
 	});
 	socket.on('accept', function(data){
 		if(data){
-		document.cookie=JSON.stringify($('#username').val());
-		window.location.href="/home";
-		}
-		else{
+			document.cookie=JSON.stringify($('#username').val());
+			window.location.href="/home";
+		} else{
 		$('#alertlocation').after('<div class="alert alert-danger alert-dismissable" id="failed"><button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button> ERROR: Invalid Login. </div>');
 		}
 	});
